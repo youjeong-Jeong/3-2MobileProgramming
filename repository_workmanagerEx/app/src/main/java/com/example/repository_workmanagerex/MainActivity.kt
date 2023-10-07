d9f3c62146b6627c8cddb534aa65f30c70c95962
youjeong Jeong <wjddbwjd1205@naver.com>
2023-10-07 23:28:51 +0900

    먼저 internet 따라하기 실습 코드 작성
    internet  따라하기 실습 코드 위에 WorkManager추가 할 거임

---
+++ repository_workmanagerEx/app/src/main/java/com/example/repository_workmanagerex/MainActivity.kt
@@ -0,0 +1,75 @@
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.repository_workmanagerex.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+data class Owner(val login: String)
+data class Repo(val name: String, val owner: Owner, val url: String)
+
+interface RestApi {
+    @GET("users/{user}/repos")
+    suspend fun listRepos(@Path("user") user: String): List<Repo>
+}
+
+class MyAdapter(val items: List<Repo>) : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+    class MyViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+        val textView = v.findViewById<TextView>(R.id.textView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val layoutInfalter = LayoutInflater.from(parent.context)
+        val view = layoutInfalter.inflate(R.layout.item_layout, parent, false)
+        val viewHolder = MyViewHolder(view)
+        return viewHolder
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.textView.text = items[position].name
+    }
+}
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+
+        val api = retrofit.create(RestApi::class.java)
+
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = MyAdapter(emptyList())
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val repos = api.listRepos("jyheo")
+            for (r in repos) {
+                println("${r.name}")
+            }
+            withContext(Dispatchers.Main) {
+                recyclerView.adapter = MyAdapter(repos)
+            }
+        }
+    }
+}
